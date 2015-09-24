f7e4c0471a843fe0120f1f439ce0703f186fd014
kevin578 <kevinbriggs1@gmail.com>
2015-09-24 18:54:52 -0400

    has if and else

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -7,18 +7,27 @@
     
     $places = preg_replace('~[+,US]~', ' ',$_GET["geo"]);
     $places = explode (' ', $places);
+
     
     
     for ($i = 0; $i < count($places); $i++){
     if( (is_numeric($places[$i])) && (strlen($places[$i]) == 5)){  
         $places = query("SELECT * FROM places WHERE postal_code = ?", $places[$i]);
+        break;
+        }
+    else {
+        if (count($places = 1)){
+        $places = query("SELECT * FROM places WHERE place_name = ?", $places[0]);
+        echo $places[0];
+        }
+
+        else if (count($places > 1)){
+            $places = query("SELECT * FROM places WHERE place_name = ? AND admin_name1 = ?", $places[0], $places[1]);
         }
     }
+    }    
     
-    else {
-        $places = query("SELECT * FROM places WHERE ")
-    
-    }
+
        
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
