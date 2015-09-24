f9f687d6f83104fe5a122273bbb63e19b9bd803a
kevin578 <kevinbriggs1@gmail.com>
2015-09-24 18:17:16 -0400

    search with if statements

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -4,13 +4,22 @@
 
     // numerically indexed array of places
     $places = [];
-
-    $places = query("SELECT * FROM places WHERE postal_code = ?",$_GET["geo"]);
+    
+    $places = preg_replace('~[+,US]~', ' ',$_GET["geo"]);
+    $places = explode (' ', $places);
     
     
+    for ($i = 0; $i < count($places); $i++){
+    if( (is_numeric($places[$i])) && (strlen($places[$i]) == 5)){  
+        $places = query("SELECT * FROM places WHERE postal_code = ?", $places[$i]);
+        }
+    }
     
+    else {
+        $places = query("SELECT * FROM places WHERE ")
     
-        
+    }
+       
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
