7a60c2267121c77839e89f23f87fdf185a1fcbe3
kevin578 <kevinbriggs1@gmail.com>
2015-11-01 08:33:11 -0500

    removeMarker finished

--- pset8/public/js/scripts.js
+++ pset8/public/js/scripts.js
@@ -198,7 +198,11 @@ function hideInfo()
  */
 function removeMarkers()
 {
-    // TODO
+    for (var i = 0; i < markers.length; i++){
+    markers[i].setMap(null);
+    markers[i] = null;
+    }
+    markers.length = 0;
 }
 
 /**
